68f89a43b353c423a82e7b0c454d688b012ca238
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-13 18:38:09 -0500

    Created an obserable with data stream concept. Used setTimeout so we can visually see this concept.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-// gets installed when we create an ng application
 import { Observable } from 'rxjs';
 
 @Component({
@@ -8,39 +7,25 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Goal, when the button is clicked we want to subscribe the observable (myObservable)
-  // And whatever value that is recieved we want to insert it inside the data array
-
   title = 'observables';
-
   data: any[] = [];
 
-
-  // 1. we create an observable to do that we need to call the Constructor of Observable
-  // 2. we assigned the object returned from the observable to a property 
-  // 3. we need to make the observable EMIT some data [so we pass a callback function]
-  //    This callback function takes in an argument (it can be named anything)
-  //    this argument will be injected by  RxJS library 
-  //    It can be seen as the subscriber 
-
-  // whenever new data emits from myObservable, the code in the getAsynch method will be notified
   // Observable
-  myObservable = new Observable((observer) =>{ // Observable is imported from RxJS library 
-    // observer is going to recieve all the subscribers that are subscribed to the myObservable property
-
-    // 4. If we want to emit some value we need to call next method on the Observer
-    observer.next([1, 2, 3, 4, 5]);  // 5. myObservable will emit the data inside the next method
+  myObservable = new Observable((observer) =>{
+    // 1 mili-seconds or 1 second (exmaple of data stream)
+    setTimeout(()=> { observer.next(1); }, 1000);
+    setTimeout(()=> { observer.next(2); }, 2000);
+    setTimeout(()=> { observer.next(3); }, 3000);
+    setTimeout(()=> { observer.next(4); }, 4000);
+    setTimeout(()=> { observer.next(5); }, 5000);
   })  
 
-  // 6. The first thing we are going to do is Subscribe to this observable using subscribe method
   GetAsynchData(){
-    // Observer
-    // this subscribe method takes 3 call back functions [next, error, complete]
-    // next function gets executed whenever the observable emits next event
-    // error call back function gets called whenever the observable emits the error event
-    // complete CB function gets called whenever the observable emits the complete event
-    this.myObservable.subscribe((val: any) => { // val will get the array in next method
-      this.data = val;  // set value (array coming in)  to empty array
+
+    // Observer/subscriber [this.myObservable.subscribe]
+    this.myObservable.subscribe((val: any) => {
+      this.data.push(val); // the call back function is the handler
     });
+
   }
 }
