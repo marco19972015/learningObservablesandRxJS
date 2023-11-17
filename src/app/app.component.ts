8d350e7b2fdca9d4767c49aa99d2591897576c22
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-17 16:43:34 -0500

    Created an observable using the of operator.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,32 +10,41 @@ export class AppComponent {
   title = 'observables';
   data: any[] = [];
 
+  array1 = [1, 3, 5, 7, 9];
+  array2 = ['A', 'B', 'C', 'D'];
+
+
+
   // Observable
-  myObservable = new Observable((observer) =>{
-    // 1 mili-seconds or 1 second (exmaple of data stream) 
-    setTimeout(()=> { observer.next(1); }, 1000);
-    setTimeout(()=> { observer.next(2); }, 2000);
-    setTimeout(()=> { observer.next(3); }, 3000);
-    // Emit an error using Error class
-    // setTimeout(()=> { observer.error(new Error('Something went wrong. Please try again later')); }, 3000);
-    // Code below will not run
-    setTimeout(()=> { observer.next(4); }, 4000);
-    setTimeout(()=> { observer.next(5); }, 5000);
-    // Once this complete method is emitted anything after it won't be emitted
-    setTimeout(()=> { observer.complete(); }, 6000);  // signals all the data from the stream has been emitted
-  })  
+  // myObservable = new Observable((observer) =>{
+  //   setTimeout(()=> { observer.next(1); }, 1000);
+  //   setTimeout(()=> { observer.next(2); }, 2000);
+  //   setTimeout(()=> { observer.next(3); }, 3000);
+  //   // Emit an error using Error class
+  //   // setTimeout(()=> { observer.error(new Error('Something went wrong. Please try again later')); }, 3000);
+  //   setTimeout(()=> { observer.next(4); }, 4000);
+  //   setTimeout(()=> { observer.next(5); }, 5000);
+  //   setTimeout(()=> { observer.complete(); }, 6000);  // signals all the data from the stream has been emitted
+  // })  
+
+  // Creating an observable using the of operator 
+  // All the values will be streamed one after the other
+  // After all the data is streamed, the of operator will emit the complete signal
+  myObservable = of(this.array1, this.array2, 20 , 30, 'hello', true);
+ 
+  // NOTE that the array is being emitted in its entirety. It is not being emitted one value at a time 
 
   GetAsynchData(){
     // Subscribe to the observable
-    this.myObservable.subscribe({  // create an object 
-      // specify the function name (next) and specify the value
-      next: (val: any) => {    // we use arrow function to get (this) keyword from outer scope
-        this.data.push(val);  // (this) keyword initially points to instance of the .subscribe function
-      },  // use comma to add to the object
-      error(err){  // the function name is error with an err object
+    this.myObservable.subscribe({  
+      next: (val: any) => {    
+        this.data.push(val);  
+        console.log(val);
+      },  
+      error(err){  
         alert(err.message);  
-      },  // third callback function is complete
-      complete(){
+      },  
+      complete(){  // Complete signal is auto emitted
         alert('All the data is streamed!');
       }
     })
