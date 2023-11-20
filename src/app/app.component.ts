4634aba9638dd6b352cb5ff1608606d03e77f63e
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-20 13:51:05 -0500

    Removed code that was no longer being used. Created an itterable observable using the from operator. From there used the pipe and map operators to tranform the data being emitted from our first overvable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,37 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Observable, from, fromEvent, of } from 'rxjs';
+import { Observable, from, fromEvent, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent {
   title = 'observables';
   data: any[] = [];
 
-
-  // Get the btn element reference from our view
-  @ViewChild('createbtn')
-  createbtn: ElementRef | undefined;
-
-  // create the button observable prop as global
-  createBtnObs: any;
+  // Each operator in RxJS is a method/function
+  // map = function/method
 
 
-  array1 = [1, 3, 5, 7, 9];
-  array2 = ['A', 'B', 'C', 'D'];
+  // Using map operator we can transform data emitted by an observable
+  // to a map operator we need to pass a callback function that returns an observable
 
-  // Observable
-  // myObservable = new Observable((observer) =>{
-  //   setTimeout(()=> { observer.next(1); }, 1000);
-  //   setTimeout(()=> { observer.next(2); }, 2000);
-  //   setTimeout(()=> { observer.next(3); }, 3000);
-  //   // Emit an error using Error class
-  //   // setTimeout(()=> { observer.error(new Error('Something went wrong. Please try again later')); }, 3000);
-  //   setTimeout(()=> { observer.next(4); }, 4000);
-  //   setTimeout(()=> { observer.next(5); }, 5000);
-  //   setTimeout(()=> { observer.complete(); }, 6000);  // signals all the data from the stream has been emitted
-  // })  
 
-  // Creating an observable using the of operator 
-  // All the values will be streamed one after the other
-  // After all the data is streamed, the of operator will emit the complete signal
-  // myObservable = of(this.array1, this.array2, 20 , 30, 'hello', true);
+  // filter operator is used to filter data emitted by a source observable based on a given condition
 
-  // each element of an ITTERABLE is emitted/streams one after the other 
-  // myObservable = from('123456789');
+  // myObservable emitting - 2, 4, 6, 8, 10;
+  // results - 10, 20, 30, 40, 50
+  myObservable = from([2, 4, 6, 8, 10]);
 
-  promiseData = new Promise((resolve, reject) => {
-    resolve([10, 20, 30, 40, 50]);
-  })
-
-  myObservable = from(this.promiseData);
+  // transform is an observable
+  // val comes from myObservable ex. first 2, second 4, third 6 ...
+  transformObs = this.myObservable.pipe(map((val) => {  // inside callback we can pass the data to transform it
+    return val * 10;
+  }))
 
   GetAsynchData(){
-    this.myObservable.subscribe({  
+    this.transformObs.subscribe({  
       next: (val: any) => {    
         this.data.push(val);  
         console.log(val);
@@ -61,36 +43,5 @@ export class AppComponent implements AfterViewInit{
         alert('All the data is streamed!');
       }
     })
-  }
-
-  // Will be our method that tracks our button and handles the observable
-  buttonClicked() {
-    let count = 0;  // counter 
-
-    // first argument is the target element 
-    // second arument is the event which we want to listen on that target element
-
-    // after creating the observable we can subscribe and assign it to a property/observer
-    this.createBtnObs = fromEvent(this.createbtn?.nativeElement, 'click')  // this line will return an observable
-                        .subscribe((data) => {  // our handler for the observable/incoming data
-                          console.log(data);
-                          // call the showItem method
-                          this.showItem(++count);  // pass the counter               
-                        });  
-  }
-
-  // Call the ngAfterViewInit LCH, because it will be called when the view of this component is fully initialized
-  // call it after we interact with it, don't call it before hand
-  ngAfterViewInit(){
-    this.buttonClicked();  // call our buttonClicked method
-  }
-
-  // JS code that will create an element in DOM
-  showItem(val: number) {
-    // we access the DOM and create a div, assign that to a property
-    let div = document.createElement('div');
-    div.innerText = 'Item' + val;  // we add a text inside that created element
-    div.className = 'data-list';
-    document.getElementById('container')?.appendChild(div);  // append the child element 
   }
 }
