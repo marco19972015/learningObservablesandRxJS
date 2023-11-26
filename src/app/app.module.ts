bd71476f1a2896e2b2dba1c4f80011d63353d6e6
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-26 17:56:51 -0500

    Added a new subject component. Will add code to show the difference between an observable and a subject.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { ShowTaskComponent } from './show-task/show-task.component';
 import { FormsModule } from '@angular/forms';
+import { SubjectComponent } from './subject/subject.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewTaskComponent,
     ShowTaskComponent,
+    SubjectComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/subject/subject.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-subject',
+  templateUrl: './subject.component.html',
+  styleUrls: ['./subject.component.css']
+})
+
+export class SubjectComponent implements OnInit{
+  ngOnInit(){
+    let obs = new Observable
+  }
+}
