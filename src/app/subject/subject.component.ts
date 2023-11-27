120a41625ff36c2e6eea86819f210cff28daa682
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-27 12:12:24 -0500

    Added an issue where we subscribe to an api three times wanting one request made, but since we are using observables we get three request made (observable is unicast). To fix this we will use subjects

--- src/app/subject/subject.component.ts
+++ src/app/subject/subject.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-subject',
@@ -10,32 +11,12 @@ import { Observable, Subject } from 'rxjs';
 export class SubjectComponent implements OnInit{
   ngOnInit(){
 
-    // Here we can see that subject is 'multicast' since it emits the same value
-    // for all the subscribers
-    const subject = new Subject();
+    // AJAX Call  (GET request) ajax method returns an observable
+    const data = ajax('https://randomuser.me/api/')
 
-    // Subscriber 1
-    subject.subscribe((data) => {console.log(data);})
-
-    // Subscriber 2
-    subject.subscribe((data) => {console.log(data);})
-
-
-
-    subject.next(Math.random());
-
-    // The reason for calling the subscribe method before the next method is to ensure that
-    // the subscribers are registered with the subject before any data is emitted. When we 
-    // call the next, it emits a new value to all the registered subscribers. If we were to call
-    // the next method before adding the subscribers, the emitted value would not have any subscribers to receive
-    // and handle it.
-
-    // We want to set up our subscriptions before triggering any events. 
-
-
-
-      // Note, an observable always provides a data (so it emits some data)
-    //       a subject can be a data provider as well as a data consumer
-
+    // In our waterfall we will see three request made (Observable = unicast)
+    data.subscribe((res) => {console.log(res);})
+    data.subscribe((res) => {console.log(res);})
+    data.subscribe((res) => {console.log(res);})
   }
 }
