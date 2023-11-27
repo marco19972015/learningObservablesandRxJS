9896fb208d751b61aad5948258e3752de9f56d6e
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-27 12:20:06 -0500

    Created an example of using subject as a consumer. This allows to make one api GET resquest when subscribing in three different instances

--- src/app/subject/subject.component.ts
+++ src/app/subject/subject.component.ts
@@ -11,12 +11,21 @@ import { ajax } from 'rxjs/ajax';
 export class SubjectComponent implements OnInit{
   ngOnInit(){
 
-    // AJAX Call  (GET request) ajax method returns an observable
+    // Create an instance of a subject observable
+    const subject = new Subject();
+
+    // AJAX Call (GET request) ajax method returns an observable
     const data = ajax('https://randomuser.me/api/')
 
     // In our waterfall we will see three request made (Observable = unicast)
-    data.subscribe((res) => {console.log(res);})
-    data.subscribe((res) => {console.log(res);})
-    data.subscribe((res) => {console.log(res);})
+    subject.subscribe((res) => {console.log(res);})
+    subject.subscribe((res) => {console.log(res);})
+    subject.subscribe((res) => {console.log(res);})
+
+    // In this instance subject is now a consumer of a value.
+    // where the ajax method returns an observable
+    // and subject consumes that observable and converts it into a subject 
+    // and passes the same data to all the subscribers
+    data.subscribe(subject);
   }
 }
