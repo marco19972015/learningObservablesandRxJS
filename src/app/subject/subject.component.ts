73ab1596ceffe24325d0dd81f8cd356bd8743b0e
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-27 12:05:22 -0500

    Created an example of how a Subject is multicast.

--- src/app/subject/subject.component.ts
+++ src/app/subject/subject.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-subject',
@@ -10,16 +10,32 @@ import { Observable } from 'rxjs';
 export class SubjectComponent implements OnInit{
   ngOnInit(){
 
-    // The example bellow shows how an Observable is unicast.
-    // It does not emit the same value for all its subscribers
-
-    // Emit a random number and from our obs observable
-    let obs = new Observable((observer) => {observer.next(Math.random())})
+    // Here we can see that subject is 'multicast' since it emits the same value
+    // for all the subscribers
+    const subject = new Subject();
 
     // Subscriber 1
-    obs.subscribe((data) => {console.log(data);})
+    subject.subscribe((data) => {console.log(data);})
 
     // Subscriber 2
-    obs.subscribe((data) => {console.log(data);})
+    subject.subscribe((data) => {console.log(data);})
+
+
+
+    subject.next(Math.random());
+
+    // The reason for calling the subscribe method before the next method is to ensure that
+    // the subscribers are registered with the subject before any data is emitted. When we 
+    // call the next, it emits a new value to all the registered subscribers. If we were to call
+    // the next method before adding the subscribers, the emitted value would not have any subscribers to receive
+    // and handle it.
+
+    // We want to set up our subscriptions before triggering any events. 
+
+
+
+      // Note, an observable always provides a data (so it emits some data)
+    //       a subject can be a data provider as well as a data consumer
+
   }
 }
