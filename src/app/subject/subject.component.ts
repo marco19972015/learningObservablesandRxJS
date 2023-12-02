184600f6f00d4873d400ce7f35f2a188e41ad985
Marco Torres <marcoantonio.torres@icloud.com>
2023-12-02 14:00:59 -0500

    Created an example of using ReplaySubject signal.

--- src/app/subject/subject.component.ts
+++ src/app/subject/subject.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -10,46 +10,26 @@ import { ajax } from 'rxjs/ajax';
 
 export class SubjectComponent implements OnInit{
   ngOnInit(){
+    
+    const subject = new ReplaySubject();
 
-    // the Subject observable does not accept any hard coded values
-    // const subject = new Subject();
+    // The past values will be emitted in the new subscribers
+    subject.next(100);
+    subject.next(200);
+    subject.next(300);
 
-    // Now we can pass an initial value that emits once there is a subscriber
-    const subject = new BehaviorSubject<number>(100);
+    // Subscriber 1 (at run 1 subject = 100, 200, 300) (second run subject = 2020)
+    subject.subscribe((data) => {console.log('Subscriber 1: ' + data);})
 
-    // Subscriber 1 (at run time data = 100) then (data = 2020)
-    subject.subscribe((data) => {console.log(data);})
+    // Subscriber 2 (at run 1 subject = 100, 200, 300) (second run subject = 2020)
+    subject.subscribe((data) => {console.log('Subscriber 2: ' + data);})
 
-    // Subscriber 2 (at run time data = 100) then (data = 2020)
-    subject.subscribe((data) => {console.log(data);})
-
-    // Once the first code runs the 2 subscribers will recieve the value 2020
     subject.next(2020);
 
-    // Subscriber 3 (at run time data = 2020 [last emitted value])
-    subject.subscribe((data) => {console.log(data);})
+    // Subscriber 3 (at run 1 subject = 100, 200, 300) (second run subject = 2020)
+    subject.subscribe((data) => {console.log('Subscriber 3: ' + data);})
 
-    // 2023 will be emitted three times, since we subscribed 3 times
-    subject.next(2023);
-
-
-
-
-    // // Create an instance of a subject observable
-    // const subject = new Subject();
-
-    // // AJAX Call (GET request) ajax method returns an observable
-    // const data = ajax('https://randomuser.me/api/')
-
-    // // In our waterfall we will see three request made (Observable = unicast)
-    // subject.subscribe((res) => {console.log(res);})
-    // subject.subscribe((res) => {console.log(res);})
-    // subject.subscribe((res) => {console.log(res);})
-
-    // // In this instance subject is now a consumer of a value.
-    // // where the ajax method returns an observable
-    // // and subject consumes that observable and converts it into a subject 
-    // // and passes the same data to all the subscribers
-    // data.subscribe(subject);
+    // At this line (subject = 2023, 2023, 2023)
+    subject.next(2023)
   }
 }
