9163b35b9faa68c98a2a486030090d64943caa6f
Marco Torres <marcoantonio.torres@icloud.com>
2023-12-05 11:12:47 -0500

    Created  a new component called unsubscribe. Showed example of how to unsubscribe from a signular observable.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NewTaskComponent } from './new-task/new-task.component';
 import { ShowTaskComponent } from './show-task/show-task.component';
 import { FormsModule } from '@angular/forms';
 import { SubjectComponent } from './subject/subject.component';
+import { UnsubscribeComponent } from './unsubscribe/unsubscribe.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SubjectComponent } from './subject/subject.component';
     NewTaskComponent,
     ShowTaskComponent,
     SubjectComponent,
+    UnsubscribeComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/subject/subject.component.ts
+++ src/app/subject/subject.component.ts
@@ -8,42 +8,42 @@ import { ajax } from 'rxjs/ajax';
   styleUrls: ['./subject.component.css']
 })
 
-export class SubjectComponent implements OnInit{
-  ngOnInit(){
-    // Promise vs Observables
+export class SubjectComponent{
+  // ngOnInit(){
+  //   // Promise vs Observables
 
-    // a promise does not need to be imported 
-    const promise = new Promise((resolve, reject) => {
-      // code below gets executed immediately since a promise ALWAYS returns some data
-      console.log('Promise is called');
-      resolve(100);
+  //   // a promise does not need to be imported 
+  //   const promise = new Promise((resolve, reject) => {
+  //     // code below gets executed immediately since a promise ALWAYS returns some data
+  //     console.log('Promise is called');
+  //     resolve(100);
 
-      // Promise won't emit these values 
-      resolve(200);
-      resolve(300);
-    })
+  //     // Promise won't emit these values 
+  //     resolve(200);
+  //     resolve(300);
+  //   })
 
-    // recieves data from what is emitted from Promise callback function
-    promise.then((data) => {
-      console.log(data);
-    })
+  //   // recieves data from what is emitted from Promise callback function
+  //   promise.then((data) => {
+  //     console.log(data);
+  //   })
 
-    // Observable needs to be imported from RxJS library
-    const obs = new Observable((data) => {
-      // code below needs a subscriber to emit (lazy)
-      console.log('Observable is called');
+  //   // Observable needs to be imported from RxJS library
+  //   const obs = new Observable((data) => {
+  //     // code below needs asubscriber to emit (lazy)
+  //     console.log('Observable is called');
       
-      // We can emit all the values below 
-      data.next(200);
-      data.next(300);
-      data.next(400);
-    });
+  //     // We can emit all the values below 
+  //     data.next(200);
+  //     data.next(300);
+  //     data.next(400);
+  //   });
 
-    // when there is a subscriber our callback function in the observable will be executed
-    obs.subscribe((data) => {
-      console.log(data);
-    });
+  //   // when there is a subscriber our callback function in the observable will be executed
+  //   obs.subscribe((data) => {
+  //     console.log(data);
+  //   });
 
 
-  }
+  // }
 }
